e5f3e168617267b32cf9c41e5f50cdd370cc9e00
LeeJaei <jaelee@a4ai.co.kr>
2025-07-14 20:11:26 +0900

    db 접속성공 및 설치 및 깃 설치 완료

--- onamushop/settings.py
+++ onamushop/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+# -*- coding: utf-8 -*-
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,8 +76,12 @@ WSGI_APPLICATION = 'onamushop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
